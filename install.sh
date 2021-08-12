3f79d4dc3847a87d16e257deae8093c2d63e3994
FORGE-JITA\ktokt <ulfnew@gmail.com>
2021-08-13 00:44:17 +0500

    Set executable flag to entrypoint while install

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@ find ./ -maxdepth 1 -type f -regextype posix-awk -regex ".*\/.*.sh$" \
     -exec chmod +x {} \;
 
 cd ./docker/ || exit 13
+chmod +x ./docker-entrypoint.sh
 cp  ./docker-compose.yml.example  ./docker-compose.yml
