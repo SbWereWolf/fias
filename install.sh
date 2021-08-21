0af690589aa14af258c74ccb6cbcdddc338fbd23
sbwerewolf <n.volkhin@big3.ru>
2021-08-21 17:09:14 +0500

    Create directory for DBMS

--- install.sh
+++ install.sh
@@ -2,6 +2,8 @@
 find ./ -maxdepth 1 -type f -regextype posix-awk -regex ".*\/.*.sh$" \
     -exec chmod +x {} \;
 
+mkdir -p ./fs/var/lib/postgresql/
+
 cd ./docker/ || exit 13
 chmod +x ./docker-entrypoint.sh
 cp  ./docker-compose.yml.example  ./docker-compose.yml
